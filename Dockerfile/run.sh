16b7747da66eb89467e702e7f5aa53e41f933aad
Favre Alexandre <xandrefavre@gmail.com>
2021-11-02 11:52:24 +0100

    add dockerfile

---
+++ Dockerfile/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Available APIs eth,miner,net,web3,personal,txpool
+echo -e "================  VARIABLES  ===================
+      DATA_DIR=${DATA_DIR:="/ethereum/"}
+      FREEZER_DIR=${FREEZER_DIR:="/ethereum/freezer"}
+      AUTHORIZED_IP=${AUTHORIZED_IP:="0.0.0.0"}
+      API_ALLOWED=${API_ALLOWED:="debug,net,eth,web3,txpool"}
+      VERBOSITY=${VERBOSITY:=1}
+      NETWORK=${NETWORK:=mainnet}
+      GCMODE=${GCMODE:=full}
+      SYNCMODE=${SYNCMODE:=full}
+      CACHE=${CACHE:=2048}
+      DEV_MODE=${DEV_MODE:=false}"
+
+if [ $NETWORK == "ropsten" ]; then
+  echo "Using ropsten config..."
+  GETH_OPTS="$GETH_OPTS --testnet "
+  echo -e "      NETWORK_ID=${NETWORK_ID:=3}"
+elif [ $NETWORK == "rinkeby" ]; then
+  echo "Using rinkeby config..."
+  GETH_OPTS="$GETH_OPTS --rinkeby "
+  echo -e "      NETWORK_ID=${NETWORK_ID:=4}"
+elif [ $NETWORK == "goerli" ]; then
+  echo "Using goerli config..."
+  GETH_OPTS="$GETH_OPTS --goerli "
+  echo -e "      NETWORK_ID=${NETWORK_ID:=5}"
+else
+  echo "Using mainnet config..."
+  echo -e "      NETWORK_ID=${NETWORK_ID:=1}"
+fi
+
+echo "================================================="
+
+if [ "x$DATA_DIR" == "x" ]; then
+  echo "DATA_DIR is undefined !"
+  echo "Exiting."
+  exit -1;
+fi
+
+if [ ! -d $DATA_DIR ]; then
+  mkdir $DATA_DIR
+fi
+
+if [ $DEV_MODE == true ]; then
+  GETH_OPTS="$GETH_OPTS --dev "
+fi
+
+NODE_LOG="$DATA_DIR/node.log"
+
+GETH_OPTS="$GETH_OPTS 
+          --syncmode=$SYNCMODE --gcmode=$GCMODE
+          --http --http.addr=$AUTHORIZED_IP --http.corsdomain '*' --http.api=$API_ALLOWED
+          --ws --ws.addr=$AUTHORIZED_IP --ws.api=$API_ALLOWED
+          --nousb
+          --datadir=$DATA_DIR --datadir.ancient=$FREEZER_DIR
+          --networkid=$NETWORK_ID
+          --verbosity=$VERBOSITY
+          --unlock=$WALLET --miner.etherbase=$WALLET"
+
+cd /root/eth-net-intelligence-api
+pm2 start ./app.json
+
+echo "geth $GETH_OPTS"
+
+geth console --exec "admin.nodeInfo.enode" > $DATA_DIR/enode.txt
+
+geth $GETH_OPTS 2>&1 >$NODE_LOG

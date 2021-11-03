4069c529951d9d5a4aaa724769ef3319c1e227ed
Alexandre Favre <Tixandre@users.noreply.github.com>
2021-11-03 21:44:29 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -39,9 +39,9 @@ echo "Build docker image"
 echo "Choose platform"
 select yn in "Server" "Raspberry-64" "Raspberry-32" "Exit"; do
     case $yn in
-        Server ) sudo docker pull ntity/blockchain:latest;version=latest;break;;
-        Raspberry-64 ) sudo docker pull ntity/blockchain:arm64v8;version=arm64v8;break;;
-        Raspberry-32 ) sudo docker pull ntity/blockchain:arm32v7;version=arm32v7;break;;
+        Server ) sudo docker pull ntity/node:latest;version=latest;break;;
+        Raspberry-64 ) sudo docker pull ntity/node:arm64v8;version=arm64v8;break;;
+        Raspberry-32 ) sudo docker pull ntity/node:arm32v7;version=arm32v7;break;;
         Exit ) exit 0;;
     esac
 done
@@ -53,21 +53,21 @@ select yn in "Create" "Existing" "Exit"; do
     case $yn in
         Create ) if [ $version = "latest" ]; then 
                 
-                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/blockchain:latest geth --datadir=/ethereum --nousb account new 
+                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/node:latest geth --datadir=/ethereum --nousb account new 
                  fi;
                  if [ $version = "arm64v8" ]; then 
                 
-                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/blockchain:arm64v8 geth --datadir=/ethereum --nousb account new 
+                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/node:arm64v8 geth --datadir=/ethereum --nousb account new 
                  fi;
                  if [ $version = "arm32v7" ]; then
                 
-                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/blockchain:arm32v7 geth --datadir=/ethereum --nousb account new 
+                    sudo docker run -it -v "/data/blockchain/ntity-01:/ethereum" ntity/node:arm32v7 geth --datadir=/ethereum --nousb account new 
                  fi;
                  echo "Please copy your wallet for next part";break;;
         Existing ) sudo mkdir ./keystore/; echo "Please copy the file into keystore and press Enter";while [ true ] ; do
                                             read -s -N 1 -t 1 key
                                             if [[ $key == $'\x0a' ]]; then
-                                                sudo cp ./keystore /data/blockchain/ntity-01/
+                                                sudo cp -r ./keystore /data/blockchain/ntity-01/
                                                 break;
                                             fi;done; break;;
         Exit ) exit 0;;
@@ -87,15 +87,15 @@ echo "We initalize the miner"
 sudo cp /data/blockchain/ntity.genesis.json /data/blockchain/ntity-01
 if [ $version = "latest" ]; then
 
-    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain:latest geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
+    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/node:latest geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
 fi; 
 if [ $version = "arm64v8" ]; then
 
-    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain:arm64v8 geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
+    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/node:arm64v8 geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
 fi; 
 if [ $version = "arm32v7" ]; then 
 
-    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain:arm32v7 geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
+    sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/node:arm32v7 geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json 
 fi; 
 
 echo "Please choose a name to your miner"
